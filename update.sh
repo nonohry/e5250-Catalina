05c0e7428236dd70ce615926f489df0545b0310f
Nono Heryana <nonoheryana@gmail.com>
2020-03-30 20:28:47 +0700

    Upload Repo

---
+++ update.sh
@@ -0,0 +1,7 @@
+git init
+git add *
+git status
+git config --global user.email "nonoheryana@gmail.com"
+git config --global user.name "Nono Heryana"
+git commit -m "Upload Repo"
+git push origin master
